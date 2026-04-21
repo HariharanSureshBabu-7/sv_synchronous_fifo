b57d58dcb13bf299c86f4c54ab34c2542b7630aa
Hariharan <hariharansb7@gmail.com>
2026-04-21 11:44:30 +0530

    Update tb.sv

--- tb.sv
+++ tb.sv
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03.03.2026 14:49:49
-// Design Name: 
-// Module Name: tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module tb;
 
