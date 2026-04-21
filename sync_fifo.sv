4ce2b9995eb5dc8e27cda2e72881efe19be729c9
Hariharan <hariharansb7@gmail.com>
2026-04-21 11:44:53 +0530

    Update sync_fifo.sv

--- sync_fifo.sv
+++ sync_fifo.sv
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03.03.2026 14:48:59
-// Design Name: 
-// Module Name: sync_fifo
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module sync_fifo #(parameter DEPTH=8, DWIDTH=16)(
         input rstn, clk, wr_en, rd_en, 			  
